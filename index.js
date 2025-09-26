710b523aca1f2dadd2e5a23b55f954e1652c4e63
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2025-09-25 21:28:31 -0400

    hora data

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function mudarFundo() {
     document.body.style.backgroundColor = document.body.style.backgroundColor === 'lightblue' ? '#f0f0f0' : 'lightblue';
 }
+function mostrarDataHora() {
+    const dataHora = new Date();
+    document.getElementById("data-hora").innerHTML = "Data e Hora Atual: " + dataHora.toLocaleString();
+}
